d7f18a3fdbb35769ad5381c5d2aedb3569c058a0
Esbjörn <ee@fortrabbit.com>
2021-04-08 20:28:22 +0200

    remove outdated phpinfo argon check

--- public/info.php
+++ public/info.php
@@ -2,15 +2,4 @@
 
 error_log('Someone visited testrabbit!');
 
-# Check for argon2 password hashing
-print '<pre>';
-if (defined('PASSWORD_ARGON2ID')) {
-    echo "We have PASSWORD_ARGON2ID! (probably PHP 7.3)\n";
-} elseif (defined('PASSWORD_ARGON2I')) {
-    echo "We have PASSWORD_ARGON2I! (probably PHP 7.2)\n";
-} else {
-    echo "No argon2 support (probably PHP 7.1)\n";
-}
-print '</pre>';
-
 phpinfo();

ac7cc68a90718dbcce916f9408c02f01b39e8aeb
Esbjörn <ee@fortrabbit.com>
2021-05-03 17:34:52 +0200

    mongo

--- config/database.php
+++ config/database.php
@@ -17,7 +17,7 @@ return [
 
 
     // Test database (credentials in the config are intentionally)
-    'mongodb' => 'mongodb+srv://testrabbit:wXS2LqpEkZNUkmfb@testrabbit.ayol0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    'mongodb' => 'mongodb+srv://testrabbit:ATXLx7b6yqwEEoQB@testrabbit.ayol0.mongodb.net/testrabbit?retryWrites=true&w=majority',
 
     /*
     |--------------------------------------------------------------------------

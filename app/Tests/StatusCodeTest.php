a015642a5f343b044e0e1ac2952e463d574d3791
Alex Pestell <ap@fortrabbit.com>
2022-04-11 10:38:22 +0200

    Refactor status code test

--- app/Tests/StatusCodeTest.php
+++ app/Tests/StatusCodeTest.php
@@ -2,9 +2,32 @@
 
 namespace App\Tests;
 
-class StatusCodeTest
+class StatusCodeTest implements Test
 {
-    protected function getStatusCode(string $url)
+    protected string $scheme;
+    protected string $url;
+    protected int $expectedStatus;
+
+    public function execute(): Result
+    {
+        try {
+            $url = $this->scheme . $_SERVER['HTTP_HOST'] . $this->url;
+            $message = $this->getStatusCode($url);
+            $success = $message == $this->expectedStatus;
+        } catch (\Exception $e) {
+            $success = false;
+            $message = $e->getMessage();
+        }
+
+        return new Result($success, $message);
+    }
+
+    public function appType(): string
+    {
+        return self::APP_UNI;
+    }
+
+    private function getStatusCode(string $url)
     {
         $handler = curl_init();
         curl_setopt($handler, CURLOPT_URL, $url);
